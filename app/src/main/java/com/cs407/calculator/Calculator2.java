7cd718825f7ca8ab537bb18655d8904db92c8530
Diana <dayang@wisc.edu>
2023-09-20 22:22:08 -0500

    calculator implementation

--- app/src/main/java/com/cs407/calculator/Calculator2.java
+++ app/src/main/java/com/cs407/calculator/Calculator2.java
@@ -2,13 +2,19 @@ package com.cs407.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Calculator2 extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator2);
+        textView=(TextView) findViewById(R.id.result);
+        Intent intent=getIntent();
+        String str=intent.getStringExtra("message");
+        textView.setText(str);
     }
 }
--- app/src/main/java/com/cs407/calculator/MainActivity.java
+++ app/src/main/java/com/cs407/calculator/MainActivity.java
@@ -2,10 +2,55 @@ package com.cs407.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public void clickAdd(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        double int1 = Integer.parseInt(myTextField.getText().toString());
+        double int2 = Integer.parseInt(myTextField2.getText().toString());
+        double sum = int1 + int2;
+        String result = Double.toString(sum);
+        goToActivity(result);
+    }
 
+    public void clickMinus(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        double int1 = Integer.parseInt(myTextField.getText().toString());
+        double int2 = Integer.parseInt(myTextField2.getText().toString());
+        String result = Double.toString(int1 - int2);
+        goToActivity(result);
+    }
+
+    public void clickMultiply(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        double int1 = Integer.parseInt(myTextField.getText().toString());
+        double int2 = Integer.parseInt(myTextField2.getText().toString());
+        String result = Double.toString(int1 * int2);
+        goToActivity(result);
+    }
+
+    public void clickDivide(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        double int1 = Integer.parseInt(myTextField.getText().toString());
+        double int2 = Integer.parseInt(myTextField2.getText().toString());
+        String result = Double.toString(int1 / int2);
+        goToActivity(result);
+
+    }
+
+    public void goToActivity(String s){
+        Intent intent = new Intent(this, Calculator2.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
